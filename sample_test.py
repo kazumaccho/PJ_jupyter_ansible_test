f7624d981f3ca4041f7b82437456b70ce0948bec
kazumaccho <phenome@icloud.com>
2025-05-12 12:57:06 +0900

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -1,45 +1,15 @@
-全体のコードをよく理解し、google styleでdocstringを生成しました。以下が結果としてのコードとdocstringです。
+class Average():
+    def __init__(self, name, japanese, english):
+        self.__name = name
+        self.__japanese = japanese
+        self.__english = english
+        self.__average = self.__calculate_average()
 
-```python
-import unittest
+    def __calculate_average(self):
+        return (self.__japanese + self.__english) / 2
 
-class TestStringMethods(unittest.TestCase):
-    """文字列操作の単体テストを行うクラス。
+    def get_name(self):
+        return self.__name
 
-    このクラスでは、文字列の基本的な操作に対する単体テストを行います。
-    テストの対象には、文字列が大文字かどうか、文字列の分割が正しく行われるか、
-    部分文字列が含まれているかどうかが含まれます。
-    """
-
-    def test_upper(self):
-        """'foo'を大文字に変換するテスト。
-
-        'foo'を大文字に変更して'FOO'となることを確認します。
-        """
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        """文字列が大文字かどうかを確認するテスト。
-
-        'FOO'が大文字であることと、'Foo'が大文字でないことを確認します。
-        """
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        """文字列を指定した区切り文字で分割するテスト。
-
-        'hello world'をスペースで分割して、['hello', 'world']となることを確認します。
-        また、区切り文字が含まれない場合はValueErrorが発生することを確認します。
-        
-        Raises:
-            ValueError: 区切り文字が含まれていない場合。
-        """
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        with self.assertRaises(ValueError):
-            s.split(',')
-
-if __name__ == '__main__':
-    unittest.main()
-```
+    def get_average(self):
+        return self.__average
