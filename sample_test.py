cf14ac4d7e43916890fc26561d57d4ec6e78e2be
kazumaccho <phenome@icloud.com>
2025-05-12 13:02:22 +0900

    Update sample_test.py

--- sample_test.py
+++ sample_test.py
@@ -1,49 +1,15 @@
-以下に「sample_test.py」のコードに対してGoogleスタイルのdocstringを追加しています。
+class Average():
+    def __init__(self, name, japanese, english):
+        self.__name = name
+        self.__japanese = japanese
+        self.__english = english
+        self.__average = self.__calculate_average()
 
-```python
-import mplfinance as mpf
-import pandas as pd
+    def __calculate_average(self):
+        return (self.__japanese + self.__english) / 2
 
-class SampleTest:
-    """
-    サンプルテストクラス
+    def get_name(self):
+        return self.__name
 
-    このクラスは、金融データの可視化と処理を行うための基本的な機能を提供します。
-    データはPandas DataFrame形式で提供され、mplfinanceライブラリを使用してローソク足チャートを作成します。
-    """
-
-    def __init__(self, data_file):
-        """
-        初期化メソッド
-
-        データファイルを読み込み、データフレームを初期化します。
-
-        Args:
-            data_file (str): データのファイルパス
-        """
-        self.data_file = data_file
-        self.data = None
-
-    def load_data(self):
-        """
-        データを読み込むメソッド
-
-        CSVファイルから金融データを読み込み、データフレームとして格納します。
-        """
-        self.data = pd.read_csv(self.data_file, index_col=0, parse_dates=True)
-
-    def plot_data(self):
-        """
-        データをプロットするメソッド
-
-        読み込んだデータを使ってローソク足チャートを作成し、表示します。
-        """
-        if self.data is not None:
-            mpf.plot(self.data, type='candle', style='charles',
-                     title='Stock Price Data', ylabel='Price',
-                     volume=True, mav=(3, 6, 9))
-        else:
-            print("データが読み込まれていません。先にload_dataメソッドを呼び出してください。")
-```
-
-この出力はコードに付加されたdocstringで完全かつ正確です。これにより、初心者でもコードの目的と使用方法を理解しやすくなります。
+    def get_average(self):
+        return self.__average
