8a66aba63843678687ede3ecb4b8681b807c37e8
kazumaccho <phenome@icloud.com>
2025-05-12 13:06:39 +0900

    Update test_sample.py with docstrings

--- sample_test.py
+++ sample_test.py
@@ -1,15 +1,39 @@
-class Average():
-    def __init__(self, name, japanese, english):
-        self.__name = name
-        self.__japanese = japanese
-        self.__english = english
-        self.__average = self.__calculate_average()
+```python
+class Greeting:
+    """
+    挨拶に関するクラスです。このクラスは異なる方法で挨拶メッセージを生成します。
+    """
 
-    def __calculate_average(self):
-        return (self.__japanese + self.__english) / 2
+    def __init__(self, name):
+        """
+        初期化メソッド。名前をセットします。
 
-    def get_name(self):
-        return self.__name
+        Args:
+            name (str): 挨拶する対象の名前。
+        """
+        self.name = name
 
-    def get_average(self):
-        return self.__average
+    def say_hello(self):
+        """
+        "Hello, {name}!" という形式の挨拶を返します。
+
+        Returns:
+            str: 挨拶メッセージ。
+        """
+        return f"Hello, {self.name}!"
+
+    def say_goodbye(self):
+        """
+        "Goodbye, {name}!" という形式の別れの挨拶を返します。
+
+        Returns:
+            str: 別れの挨拶メッセージ。
+        """
+        return f"Goodbye, {self.name}!"
+
+
+if __name__ == "__main__":
+    greeting = Greeting("World")
+    print(greeting.say_hello())
+    print(greeting.say_goodbye())
+```
